a661f67d9afb0170931a4e51440b6408d4cbe22d
Druidan <44009123+Druidan@users.noreply.github.com>
2019-06-02 23:25:02 -0500

    ran npm build

---
+++ build/precache-manifest.1d48173fea40b82365a483e681a69698.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b39eac21d29ca083a47ebbd348c6ca1f",
+    "url": "/Druidan/GameMole/index.html"
+  },
+  {
+    "revision": "f22873c559232eb794a5",
+    "url": "/Druidan/GameMole/static/css/main.1a24ef98.chunk.css"
+  },
+  {
+    "revision": "d4c14d2672a5478a93b5",
+    "url": "/Druidan/GameMole/static/js/2.0b62ab56.chunk.js"
+  },
+  {
+    "revision": "f22873c559232eb794a5",
+    "url": "/Druidan/GameMole/static/js/main.b7a1c40d.chunk.js"
+  },
+  {
+    "revision": "8c4e040a56aa6b93638a",
+    "url": "/Druidan/GameMole/static/js/runtime~main.fab60fea.js"
+  }
+]);
