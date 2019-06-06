fbbeb6c9ae8bb15035b42f058eb429a2eb5fc33f
Druidan <44009123+Druidan@users.noreply.github.com>
2019-06-05 21:57:24 -0500

    health check

--- routes/routes.js
+++ routes/routes.js
@@ -11,111 +11,115 @@ const db = require('../models');
 
 //-------------------------------------
 
+router.get('/healthcheck', function (req, res) {
+    res.send('healthy!');
+})
+
 //Scraper Router
 router.get('/scrape', function (req, res) {
     axios.all([
-        axios.get('https://www.ign.com/articles?tags=news'),
-        axios.get('https://www.gameinformer.com/news'),
-        axios.get('https://www.destructoid.com/'),
-    ])
-    .then(axios.spread( (ignResponse, giResponse, destResponse) => {
-        const ign$ = cheerio.load(ignResponse.data);
-        const gi$ = cheerio.load(giResponse.data);
-        const dest$ = cheerio.load(destResponse.data);
-        let allResults = {};
-        let savedSummaries = [];
+            axios.get('https://www.ign.com/articles?tags=news'),
+            axios.get('https://www.gameinformer.com/news'),
+            axios.get('https://www.destructoid.com/'),
+        ])
+        .then(axios.spread((ignResponse, giResponse, destResponse) => {
+            const ign$ = cheerio.load(ignResponse.data);
+            const gi$ = cheerio.load(giResponse.data);
+            const dest$ = cheerio.load(destResponse.data);
+            let allResults = {};
+            let savedSummaries = [];
 
-        const trueRes = res;
+            const trueRes = res;
 
-        return db.Article.find({})
-            .then(res => {
-                const savedArticles = res;
-                for(let article in savedArticles){
-                    savedSummaries.push((savedArticles[article].summary));
-                }
-
-                ign$('div.listElmnt').each( (i, element) => {
-        
-                    i = i+1;
-                    const ignResult = {};
-                    ignResult[`ignArticle ${i}`] = {};
-                    ignResult[`ignArticle ${i}`].title = ign$(element)
-                        .find('div.listElmnt-blogItem')
-                        .find('a.listElmnt-storyHeadline')
-                        .text();
-                    ignResult[`ignArticle ${i}`].link = ign$(element)
-                        .find('div.listElmnt-blogItem')
-                        .find('a.listElmnt-storyHeadline')
-                        .attr('href');
-                    ignResult[`ignArticle ${i}`].source = 'IGN';
-                    ignResult[`ignArticle ${i}`].sourceRef = 'https://www.ign.com';
-                    ignResult[`ignArticle ${i}`].logo = 'IGNlogo.png';
-                    sum = ign$(element)
-                        .find('p')
-                        .text()
-                        .match(/(?<=-)[\s\S]*(?=Read)/g);
-                    ignResult[`ignArticle ${i}`].summary = sum.join('').trim();
-                    if (!savedSummaries.includes(ignResult[`ignArticle ${i}`].summary)) {
-                        allResults = Object.assign(allResults, ignResult);
+            return db.Article.find({})
+                .then(res => {
+                    const savedArticles = res;
+                    for (let article in savedArticles) {
+                        savedSummaries.push((savedArticles[article].summary));
                     }
+
+                    ign$('div.listElmnt').each((i, element) => {
+
+                        i = i + 1;
+                        const ignResult = {};
+                        ignResult[`ignArticle ${i}`] = {};
+                        ignResult[`ignArticle ${i}`].title = ign$(element)
+                            .find('div.listElmnt-blogItem')
+                            .find('a.listElmnt-storyHeadline')
+                            .text();
+                        ignResult[`ignArticle ${i}`].link = ign$(element)
+                            .find('div.listElmnt-blogItem')
+                            .find('a.listElmnt-storyHeadline')
+                            .attr('href');
+                        ignResult[`ignArticle ${i}`].source = 'IGN';
+                        ignResult[`ignArticle ${i}`].sourceRef = 'https://www.ign.com';
+                        ignResult[`ignArticle ${i}`].logo = 'IGNlogo.png';
+                        sum = ign$(element)
+                            .find('p')
+                            .text()
+                            .match(/(?<=-)[\s\S]*(?=Read)/g);
+                        ignResult[`ignArticle ${i}`].summary = sum.join('').trim();
+                        if (!savedSummaries.includes(ignResult[`ignArticle ${i}`].summary)) {
+                            allResults = Object.assign(allResults, ignResult);
+                        }
+                    });
+
+                    gi$('article.node--type-article').each((i, element) => {
+                        i = i + 1;
+                        const giResult = {};
+                        giResult[`giArticle ${i}`] = {};
+                        giResult[`giArticle ${i}`].title = gi$(element)
+                            .find('span.field--name-title')
+                            .text();
+                        giResult[`giArticle ${i}`].link = gi$(element)
+                            .find('h2.page-title')
+                            .find('a')
+                            .attr('href');
+                        giResult[`giArticle ${i}`].source = 'Game Informer';
+                        giResult[`giArticle ${i}`].sourceRef = 'https://www.gameinformer.com/';
+                        giResult[`giArticle ${i}`].logo = 'GIlogo.png';
+                        giResult[`giArticle ${i}`].summary = gi$(element)
+                            .find('div.field--name-field-promo-summary')
+                            .text();
+                        if (!savedSummaries.includes(giResult[`giArticle ${i}`].summary)) {
+                            allResults = Object.assign(allResults, giResult);
+                        }
+
+                    })
+
+                    dest$('article.smlpost').each((i, element) => {
+                        i = i + 1;
+                        const destResult = {};
+                        destResult[`destArticle ${i}`] = {};
+                        destResult[`destArticle ${i}`].title = dest$(element)
+                            .find('h2.sparticle_title')
+                            .find('a')
+                            .text();
+                        const newLink = dest$(element)
+                            .find('h2.sparticle_title')
+                            .find('a')
+                            .attr('href');
+                        destResult[`destArticle ${i}`].link = `https://www.destructoid.com/${newLink}`;
+                        destResult[`destArticle ${i}`].source = 'Destructoid';
+                        destResult[`destArticle ${i}`].sourceRef = 'https://www.destructoid.com/';
+                        destResult[`destArticle ${i}`].logo = 'Destlogo.png';
+                        destResult[`destArticle ${i}`].summary = dest$(element)
+                            .find('p')
+                            .text();
+                        if (!savedSummaries.includes(destResult[`destArticle ${i}`].summary)) {
+                            if (destResult[`destArticle ${i}`].title !== '') {
+                                allResults = Object.assign(allResults, destResult);
+                            }
+                        }
+                    })
+
+                    trueRes.json(allResults);
+
+                })
+                .catch(err => {
+                    console.log("We ran into problems getting saved titles!")
+                    console.log(err);
                 });
-        
-                gi$('article.node--type-article').each( (i, element) => {
-                    i = i+1;
-                    const giResult = {};
-                    giResult[`giArticle ${i}`] = {};
-                    giResult[`giArticle ${i}`].title = gi$(element)
-                        .find('span.field--name-title')
-                        .text();
-                    giResult[`giArticle ${i}`].link = gi$(element)
-                        .find('h2.page-title')
-                        .find('a')
-                        .attr('href');
-                    giResult[`giArticle ${i}`].source = 'Game Informer';
-                    giResult[`giArticle ${i}`].sourceRef = 'https://www.gameinformer.com/';
-                    giResult[`giArticle ${i}`].logo = 'GIlogo.png';
-                    giResult[`giArticle ${i}`].summary = gi$(element)
-                    .find('div.field--name-field-promo-summary')
-                    .text();
-                    if (!savedSummaries.includes(giResult[`giArticle ${i}`].summary)){
-                        allResults = Object.assign(allResults, giResult);
-                    }
-                    
-                })
-        
-                dest$('article.smlpost').each( (i, element) => {
-                    i = i+1;
-                    const destResult = {};
-                    destResult[`destArticle ${i}`] = {};
-                    destResult[`destArticle ${i}`].title = dest$(element)
-                        .find('h2.sparticle_title')
-                        .find('a')
-                        .text();
-                    const newLink = dest$(element)
-                        .find('h2.sparticle_title')
-                        .find('a')
-                        .attr('href');
-                    destResult[`destArticle ${i}`].link = `https://www.destructoid.com/${newLink}`;
-                    destResult[`destArticle ${i}`].source = 'Destructoid';
-                    destResult[`destArticle ${i}`].sourceRef = 'https://www.destructoid.com/';
-                    destResult[`destArticle ${i}`].logo = 'Destlogo.png';
-                    destResult[`destArticle ${i}`].summary = dest$(element)
-                        .find('p')
-                        .text();
-                    if (!savedSummaries.includes(destResult[`destArticle ${i}`].summary)) {
-                        if(destResult[`destArticle ${i}`].title !== ''){
-                            allResults = Object.assign(allResults, destResult);
-                        }            
-                    }
-                })
-        
-                trueRes.json(allResults);
-
-            })
-            .catch(err => {
-                console.log("We ran into problems getting saved titles!")
-                console.log(err); 
-            });
         })).catch(err => {
             console.log("We've got a problem with the scrape call, captain!")
             console.log(err);
@@ -155,41 +159,46 @@ router.get('/findComments', function (req, res) {
 //Save an article to the database
 router.post('/saveArt', function (req, res) {
     db.Article.create(req.body)
-    .then(dbArticle => {
-        res.end();
-    })
-    .catch(err => {
-        console.log("We've got a problem, captain! The save route failed!")
-        console.log(err);
-    });
+        .then(dbArticle => {
+            res.end();
+        })
+        .catch(err => {
+            console.log("We've got a problem, captain! The save route failed!")
+            console.log(err);
+        });
 });
 
 //-------------------------------------
 
 //Update an article with a new note
 router.post('/makeComment', function (req, res) {
-    const comment = {message: req.body.message};
+    const comment = {
+        message: req.body.message
+    };
     const article = req.body.articleId;
     db.Note.create(comment)
-    .then(dbNote => { 
-        console.log('The comment is away to the database, captain!')
-        console.log(dbNote)
-        db.Article.findOneAndUpdate(
-            {_id: article},
-            { $push: { notes: dbNote._id } },
-        ).then(data => {
-            console.log('Update has been sent to the article, Captain!')
-            res.end();
+        .then(dbNote => {
+            console.log('The comment is away to the database, captain!')
+            console.log(dbNote)
+            db.Article.findOneAndUpdate({
+                    _id: article
+                }, {
+                    $push: {
+                        notes: dbNote._id
+                    }
+                }, ).then(data => {
+                    console.log('Update has been sent to the article, Captain!')
+                    res.end();
+                })
+                .catch(err => {
+                    console.log("We've got a problem, captain! The update Article route failed!")
+                    console.log(err);
+                });
         })
         .catch(err => {
-            console.log("We've got a problem, captain! The update Article route failed!")
+            console.log("We've got a problem, captain! The create comment route failed!")
             console.log(err);
         });
-    })
-    .catch(err => {
-        console.log("We've got a problem, captain! The create comment route failed!")
-        console.log(err);
-    });
 })
 
 
@@ -200,14 +209,14 @@ router.post('/makeComment', function (req, res) {
 router.delete('/delArt', function (req, res) {
     console.log(req.body)
     db.Article.deleteOne(req.body)
-    .then(data => {
-        console.log(data);
-        res.end();
-    })
-    .catch(err => {
-        console.log("We've got a problem, captain! The delete route failed!")
-        console.log(err);
-    });
+        .then(data => {
+            console.log(data);
+            res.end();
+        })
+        .catch(err => {
+            console.log("We've got a problem, captain! The delete route failed!")
+            console.log(err);
+        });
 });
 
 //-------------------------------------
@@ -215,26 +224,33 @@ router.delete('/delArt', function (req, res) {
 //Remove a comment from the database
 
 router.delete('/delComm', function (req, res) {
-    const commentId = { _id: req.body._id };
-    const articleId = { _id: req.body.article};
+    const commentId = {
+        _id: req.body._id
+    };
+    const articleId = {
+        _id: req.body.article
+    };
     db.Note.findOneAndRemove(commentId)
-    .then(data => {
-        db.Article.findOneAndUpdate(
-            { _id: articleId },
-            { $pull: { notes: req.body._id } }
-        ).then(data => {
-            console.log('The command to delete the comment id has been sent to the article, Captain!')
-            res.end();
+        .then(data => {
+            db.Article.findOneAndUpdate({
+                    _id: articleId
+                }, {
+                    $pull: {
+                        notes: req.body._id
+                    }
+                }).then(data => {
+                    console.log('The command to delete the comment id has been sent to the article, Captain!')
+                    res.end();
+                })
+                .catch(err => {
+                    console.log("We've got a problem, captain! The delete comment id from Article route failed!")
+                    console.log(err);
+                });
         })
         .catch(err => {
-            console.log("We've got a problem, captain! The delete comment id from Article route failed!")
+            console.log("We've got a problem, captain! The delete comment route failed!")
             console.log(err);
         });
-    })
-    .catch(err => {
-        console.log("We've got a problem, captain! The delete comment route failed!")
-        console.log(err);
-    });
 });
 
 //-------------------------------------
