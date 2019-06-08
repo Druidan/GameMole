52eddc66694372fc9f5087716aba779b4b2df12c
Druidan <44009123+Druidan@users.noreply.github.com>
2019-06-08 14:28:30 -0500

    test

---
+++ build/precache-manifest.c97a38983a2d1fa30b2f773641ca69ee.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e38b03d4aa6e3c0a62c4ffe67947cd51",
+    "url": "/Druidan/GameMole/index.html"
+  },
+  {
+    "revision": "2d6c348a33177ea92393",
+    "url": "/Druidan/GameMole/static/css/main.367137cb.chunk.css"
+  },
+  {
+    "revision": "c9df744f584144c1c752",
+    "url": "/Druidan/GameMole/static/js/2.73812db5.chunk.js"
+  },
+  {
+    "revision": "2d6c348a33177ea92393",
+    "url": "/Druidan/GameMole/static/js/main.d4127383.chunk.js"
+  },
+  {
+    "revision": "8c4e040a56aa6b93638a",
+    "url": "/Druidan/GameMole/static/js/runtime~main.fab60fea.js"
+  }
+]);
--- server.js
+++ server.js
@@ -16,7 +16,9 @@ app.use('/', routes);
 
 if (process.env.NODE_ENV = 'development') {
     app.use(express.static('public'));
+    console.log('using public')
 } else if (process.env.NODE_ENV = 'production') {
+    console.log('using build')
     app.use(express.static(path.join(__dirname, 'build')));
 }
 
